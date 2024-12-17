2fd288f4b169cdfbccd0bf4a55a35fc8c3beeec6
Daniel Flores <danielfloresdiaz17@gmail.com>
2024-12-17 01:28:37 +0100

    chore: update init_postgres.sh to enable detailed logging

--- init_postgres.sh
+++ init_postgres.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+psql -U postgres -c "ALTER SYSTEM SET log_statement = 'all';"
+psql -U postgres -c "ALTER SYSTEM SET log_min_messages = 'NOTICE';"
+psql -U postgres -c "SELECT pg_reload_conf();"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER supabase_admin LOGIN CREATEROLE CREATEDB REPLICATION BYPASSRLS;
 
