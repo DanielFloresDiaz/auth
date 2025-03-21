aadc49f33b1676bd433edea9dfed3e2a3a6aff64
Daniel Flores <danielfloresdiaz17@gmail.com>
2025-03-21 10:52:22 +0100

    feat: add SQL initialization script for Google Cloud SQL roles and privileges

---
+++ .gcloud/gcloud_sql_init.sql
@@ -0,0 +1,28 @@
+-- Create roles with appropriate privileges
+CREATE ROLE auth_admin_role NOINHERIT;
+CREATE ROLE solomon_role NOINHERIT;
+
+-- Create schema
+CREATE SCHEMA IF NOT EXISTS auth;
+
+-- Create pgcrypto extension
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- Assign privileges to roles
+GRANT CREATE ON DATABASE postgres_auth TO auth_admin_role;
+GRANT ALL PRIVILEGES ON SCHEMA public TO auth_admin_role;
+GRANT ALL PRIVILEGES ON SCHEMA auth TO auth_admin_role;
+
+-- Grant usage permissions to solomon_role
+GRANT USAGE ON SCHEMA auth TO solomon_role;
+
+-- Grant roles to existing users
+-- Note: These users must already exist in Google Cloud SQL
+GRANT auth_admin_role TO auth_admin;
+
+-- Set search paths for existing users
+ALTER USER auth_admin SET search_path TO auth;
+
+-- Grant these roles to existing users if needed
+-- GRANT solomon_role TO solomon_admin;
+-- GRANT solomon_role TO solomon_user;
