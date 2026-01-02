2686c5cae079d6bcb8833251de9badb45ef67a22
Daniel Flores <danielfloresdiaz17@gmail.com>
2026-01-02 12:48:02 +0100

    fix(init_postgres): correct role creation and permissions for solomon roles

--- init_postgres.sh
+++ init_postgres.sh
@@ -13,22 +13,26 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON SCHEMA public TO $AUTH_DB_ADMIN;
 
     -- Create roles first
-    CREATE ROLE solomon_role;
+    CREATE ROLE solomon_user_role;
+    CREATE ROLE solomon_admin_role;
+    GRANT solomon_user_role TO solomon_admin_role;
     
     -- Grant permissions on schema level
-    GRANT USAGE ON SCHEMA $DB_NAMESPACE TO solomon_role;
+    GRANT USAGE ON SCHEMA $DB_NAMESPACE TO solomon_user_role;
     
     -- Create users and assign to roles
     CREATE USER zion_user INHERIT LOGIN NOREPLICATION PASSWORD '$ZION_DB_USER_PASSWORD';
-    CREATE USER zion_admin INHERIT LOGIN NOREPLICATION PASSWORD '$ZION_DB_ADMIN_PASSWORD';
+    CREATE USER zion_admin INHERIT LOGIN NOREPLICATION BYPASSRLS PASSWORD '$ZION_DB_ADMIN_PASSWORD';
     CREATE USER brawler_user INHERIT LOGIN NOREPLICATION PASSWORD '$BRAWLER_DB_USER_PASSWORD';
-    CREATE USER brawler_admin INHERIT LOGIN NOREPLICATION PASSWORD '$BRAWLER_DB_ADMIN_PASSWORD';
+    CREATE USER brawler_admin INHERIT LOGIN NOREPLICATION BYPASSRLS PASSWORD '$BRAWLER_DB_ADMIN_PASSWORD';
     
     -- Assign users to roles
-    GRANT solomon_role TO zion_user, brawler_user, zion_admin, brawler_admin;
+    GRANT solomon_user_role TO zion_user, brawler_user;
+    GRANT solomon_admin_role TO zion_admin, brawler_admin;
     
     -- Set search paths for existing users and roles
-    ALTER ROLE solomon_role SET search_path TO $DB_NAMESPACE;
+    ALTER ROLE solomon_user_role SET search_path TO $DB_NAMESPACE;
+    ALTER ROLE solomon_admin_role SET search_path TO $DB_NAMESPACE;
     ALTER ROLE $AUTH_DB_ADMIN SET search_path TO $DB_NAMESPACE;
     ALTER ROLE supabase_admin SET search_path TO $DB_NAMESPACE;
     ALTER ROLE zion_user SET search_path TO $DB_NAMESPACE;
