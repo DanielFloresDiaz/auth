4dccf29427627c1d75b54c7aff0eecf0e6529fbf
Daniel Flores <danielfloresdiaz17@gmail.com>
2026-01-14 05:34:00 +0100

    feat(sql): update role definitions and permissions for improved access control

--- .gcloud/gcloud_sql_init.sql
+++ .gcloud/gcloud_sql_init.sql
@@ -1,8 +1,8 @@
 -- Create roles with appropriate privileges
 CREATE ROLE auth_admin_role NOINHERIT;
-CREATE ROLE solomon_user_role;
-CREATE ROLE solomon_admin_role;
-GRANT solomon_user_role TO solomon_admin_role;
+CREATE ROLE solomon_auth_user_role;
+CREATE ROLE solomon_auth_admin_role;
+CREATE ROLE rl_auth_user_role;
 
 -- Create schema
 CREATE SCHEMA IF NOT EXISTS auth;
@@ -15,8 +15,10 @@ GRANT CREATE ON DATABASE postgres_auth TO auth_admin_role;
 GRANT ALL PRIVILEGES ON SCHEMA public TO auth_admin_role;
 GRANT ALL PRIVILEGES ON SCHEMA auth TO auth_admin_role;
 
--- Grant usage permissions to solomon_user_role
-GRANT USAGE ON SCHEMA auth TO solomon_user_role;
+-- Grant usage permissions to solomon_auth_user_role
+GRANT USAGE ON SCHEMA auth TO solomon_auth_user_role;
+GRANT USAGE ON SCHEMA auth TO solomon_auth_admin_role;
+GRANT USAGE ON SCHEMA auth TO rl_auth_user_role;
 
 -- Grant roles to existing users
 -- Note: These users must already exist in Google Cloud SQL
@@ -24,13 +26,14 @@ GRANT auth_admin_role TO auth_admin;
 
 -- Set search paths for existing users
 ALTER ROLE auth_admin SET search_path TO auth;
-ALTER ROLE solomon_user_role SET search_path TO auth;
-ALTER ROLE solomon_admin_role SET search_path TO auth;
+ALTER ROLE solomon_auth_user_role SET search_path TO auth;
+ALTER ROLE solomon_auth_admin_role SET search_path TO auth;
+ALTER ROLE rl_auth_user_role SET search_path TO auth;
 
 -- Alter role auth_admin BYPASSRLS;
 ALTER ROLE auth_admin BYPASSRLS;
 
 
 -- Grant these roles to existing users if needed
--- GRANT solomon_admin_role TO solomon_admin;
--- GRANT solomon_user_role TO solomon_user;
+-- GRANT solomon_auth_user_role TO solomon_user;
+-- GRANT solomon_auth_admin_role TO solomon_admin;
