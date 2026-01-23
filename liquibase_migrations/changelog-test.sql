8fdcf6a556a7c14ca8ca829851f36cba902a4753
Daniel Flores <danielfloresdiaz17@gmail.com>
2026-01-23 19:37:28 +0100

    fix(sql): add schema creation and permissions for auth in migration script

--- liquibase_migrations/changelog-test.sql
+++ liquibase_migrations/changelog-test.sql
@@ -2,6 +2,12 @@
 
 --changeset solomon.auth:1 labels:auth context:auth
 --comment: initial setup
+-- Set search paths for existing users and roles
+-- Create schema and grant permissions on schema level
+CREATE SCHEMA IF NOT EXISTS auth AUTHORIZATION auth_admin;
+GRANT CREATE ON DATABASE postgres TO auth_admin;
+GRANT CREATE, USAGE ON SCHEMA public TO auth_admin;
+ALTER ROLE auth_admin SET search_path TO auth;
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE ROLE solomon_auth_admin_role;
 CREATE ROLE solomon_auth_user_role;
