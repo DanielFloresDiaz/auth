4cb0a2cc2c14c9eb4bcaa05cc042110d437a9a91
Daniel Flores <danielfloresdiaz17@gmail.com>
2026-01-23 18:46:06 +0100

    fix(sql): remove unnecessary user and role creation in migration script

--- liquibase_migrations/changelog-test.sql
+++ liquibase_migrations/changelog-test.sql
@@ -2,19 +2,11 @@
 
 --changeset solomon.auth:1 labels:auth context:auth
 --comment: initial setup
-CREATE USER supabase_admin LOGIN CREATEROLE CREATEDB REPLICATION BYPASSRLS;
-CREATE USER auth_admin CREATEROLE LOGIN NOREPLICATION PASSWORD 'root';
-CREATE SCHEMA IF NOT EXISTS auth AUTHORIZATION auth_admin;
-GRANT CREATE ON DATABASE postgres TO auth_admin;
-GRANT CREATE, USAGE ON SCHEMA public TO auth_admin;
-ALTER ROLE auth_admin SET search_path TO auth;
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE ROLE solomon_auth_admin_role;
 CREATE ROLE solomon_auth_user_role;
-CREATE ROLE rl_auth_user_role;
 GRANT USAGE ON SCHEMA auth TO solomon_auth_admin_role;
 GRANT USAGE ON SCHEMA auth TO solomon_auth_user_role;
-GRANT USAGE ON SCHEMA auth TO rl_auth_user_role;
 GRANT solomon_auth_admin_role TO auth_admin;
 GRANT solomon_auth_user_role TO solomon_auth_admin_role;
---rollback DROP ROLE rl_auth_user_role; DROP ROLE solomon_auth_user_role; DROP ROLE solomon_auth_admin_role; DROP USER auth_admin; DROP USER supabase_admin; DROP SCHEMA IF EXISTS auth CASCADE; DROP EXTENSION IF EXISTS "pgcrypto";
+--rollback DROP ROLE solomon_auth_admin_role; DROP ROLE solomon_auth_user_role;
