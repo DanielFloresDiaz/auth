704d6e37b8c39e2d1d17ddc75b27a2121c55a650
Daniel Flores <danielfloresdiaz17@gmail.com>
2026-01-23 19:05:26 +0100

    fix(sql): add missing role and grant usage for rl_auth_user_role in migration script

--- liquibase_migrations/changelog-test.sql
+++ liquibase_migrations/changelog-test.sql
@@ -5,8 +5,12 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE ROLE solomon_auth_admin_role;
 CREATE ROLE solomon_auth_user_role;
+CREATE ROLE rl_auth_user_role;
 GRANT USAGE ON SCHEMA auth TO solomon_auth_admin_role;
 GRANT USAGE ON SCHEMA auth TO solomon_auth_user_role;
+GRANT USAGE ON SCHEMA auth TO rl_auth_user_role;
 GRANT solomon_auth_admin_role TO auth_admin;
 GRANT solomon_auth_user_role TO solomon_auth_admin_role;
---rollback DROP ROLE solomon_auth_admin_role; DROP ROLE solomon_auth_user_role;
+--rollback DROP ROLE solomon_auth_admin_role;
+--rollback DROP ROLE solomon_auth_user_role;
+--rollback DROP ROLE rl_auth_user_role;
