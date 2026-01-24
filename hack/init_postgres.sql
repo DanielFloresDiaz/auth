6a6270fb96694b6f2e505fcdc21cd399849e9ff0
Daniel Flores <danielfloresdiaz17@gmail.com>
2026-01-24 11:22:28 +0100

    fix(sql): update PostgreSQL initialization script to add new roles and grant permissions

--- hack/init_postgres.sql
+++ hack/init_postgres.sql
@@ -4,4 +4,21 @@ CREATE USER supabase_admin LOGIN CREATEROLE CREATEDB REPLICATION BYPASSRLS;
 CREATE USER auth_admin NOINHERIT CREATEROLE LOGIN NOREPLICATION PASSWORD 'root';
 CREATE SCHEMA IF NOT EXISTS auth AUTHORIZATION auth_admin;
 GRANT CREATE ON DATABASE postgres TO auth_admin;
+GRANT CREATE, USAGE ON SCHEMA public TO auth_admin;
 ALTER USER auth_admin SET search_path = 'auth';
+
+-- Create pgcrypto extension with superuser privileges
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- Define custom roles for application use
+CREATE ROLE solomon_auth_admin_role;
+CREATE ROLE solomon_auth_user_role;
+CREATE ROLE rl_auth_user_role;
+
+-- Grant usage permissions to custom roles
+GRANT USAGE ON SCHEMA auth TO solomon_auth_admin_role;
+GRANT USAGE ON SCHEMA auth TO solomon_auth_user_role;
+GRANT USAGE ON SCHEMA auth TO rl_auth_user_role;
+
+GRANT solomon_auth_admin_role TO auth_admin;
+GRANT solomon_auth_user_role TO solomon_auth_admin_role;
